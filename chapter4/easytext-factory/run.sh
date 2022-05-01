5945867c5a4adb7c7ec2efcbc8c7d7bbbcd34862
Thimoty Barbieri <thimoty@thimoty.it>
2022-05-01 19:14:23 +0200

    add fx mods

--- chapter4/easytext-factory/run.sh
+++ chapter4/easytext-factory/run.sh
@@ -1,4 +1,4 @@
 mkdir -p out
 
-$JAVA_HOME/bin/javac -d out --module-source-path src -m easytext.cli,easytext.gui,easytext.analysis.api,easytext.analysis.coleman,easytext.analysis.factory,easytext.analysis.kincaid
-$JAVA_HOME/bin/java --module-path out -m easytext.cli/javamodularity.easytext.cli.Main ../exampletext.txt
+javac -d out --module-source-path src --module-path ../../chapter3/easytext-threemodules/javafx-jmods-18.0.1 -m easytext.cli,easytext.gui,easytext.analysis.api,easytext.analysis.coleman,easytext.analysis.factory,easytext.analysis.kincaid
+java --module-path out -m easytext.cli/javamodularity.easytext.cli.Main ../exampletext.txt
