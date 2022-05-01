ba4afc47a2a9fe0fccd8f417e38dc4c2e8773174
Thimoty Barbieri <thimoty@thimoty.it>
2022-05-01 18:38:20 +0200

    added external mods javafx

--- chapter3/easytext-threemodules/run.sh
+++ chapter3/easytext-threemodules/run.sh
@@ -1,4 +1,4 @@
 mkdir -p out
 
-$JAVA_HOME/bin/javac -d out --module-source-path src -m easytext.cli,easytext.gui,easytext.analysis
-$JAVA_HOME/bin/java --module-path out -m easytext.cli/javamodularity.easytext.cli.Main
+javac -d out --module-source-path src --module-path javafx-jmods-18.0.1 -m easytext.cli,easytext.gui,easytext.analysis
+java --module-path out -m easytext.cli/javamodularity.easytext.cli.Main
