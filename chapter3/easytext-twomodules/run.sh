aff980192cc966d19d099afed4edad71b7b5b227
Thimoty Barbieri <thimoty@thimoty.it>
2022-05-01 18:25:36 +0200

    added file

--- chapter3/easytext-twomodules/run.sh
+++ chapter3/easytext-twomodules/run.sh
@@ -1,4 +1,4 @@
 mkdir -p out
 
-$JAVA_HOME/bin/javac -d out --module-source-path src -m easytext.cli,easytext.analysis
-$JAVA_HOME/bin/java --module-path out -m easytext.cli/javamodularity.easytext.cli.Main
+javac -d out --module-source-path src -m easytext.cli,easytext.analysis
+java --module-path out -m easytext.cli/javamodularity.easytext.cli.Main ../../chapter4/exampletext.txt
