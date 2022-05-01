df46204456e19651976162586b8c200aec618a86
Thimoty Barbieri <thimoty@thimoty.it>
2022-05-01 19:49:45 +0200

    for windows

--- chapter8/jackson-classpath/run.sh
+++ chapter8/jackson-classpath/run.sh
@@ -2,6 +2,14 @@ CP=lib/jackson-annotations-2.8.8.jar:
 CP+=lib/jackson-core-2.8.8.jar:
 CP+=lib/jackson-databind-2.8.8.jar
 
-javac -cp $CP -d out -sourcepath src $(find src -name '*.java')
+# Unix
+javac -cp lib/jackson-annotations-2.8.8.jar:lib/jackson-core-2.8.8.jar:lib/jackson-databind-2.8.8.jar -d out -sourcepath src $(find src -name '*.java')
+
+javac -cp lib/jackson-databind-2.8.8.jar:lib/jackson-core-2.8.8.jar -d out -sourcepath src $(find src -name '*.java')
 
 java -cp $CP:out demo.Main
+
+#Windows
+javac -cp lib/jackson-annotations-2.8.8.jar;lib/jackson-core-2.8.8.jar;lib/jackson-databind-2.8.8.jar -d out -sourcepath src src/demo/Book.java src/demo/Main.java
+
+java -cp lib/jackson-annotations-2.8.8.jar;lib/jackson-core-2.8.8.jar;lib/jackson-databind-2.8.8.jar;./out demo.Main
