2a9549e55adc962cb0d1e3c724596165e07a5102
Thimoty Barbieri <thimoty@thimoty.it>
2022-05-01 19:48:54 +0200

    added for windows

--- chapter8/jackson/run.sh
+++ chapter8/jackson/run.sh
@@ -1,6 +1,11 @@
 CP=lib/jackson-annotations-2.8.8.jar:
 CP+=lib/jackson-core-2.8.8.jar
 
-$JAVA_HOME/bin/javac -cp $CP --module-path mods -d out --module-source-path src -m books
+javac -cp lib/jackson-annotations-2.8.8.jar;lib/jackson-core-2.8.8.jar --module-path mods -d out --module-source-path src -m books
 
-$JAVA_HOME/bin/java -cp $CP --module-path mods:out -m books/demo.Main
+#Unix
+java -cp lib/jackson-annotations-2.8.8.jar:lib/jackson-core-2.8.8.jar --module-path mods:out -m books/demo.Main
+
+#Windows
+java -cp lib/jackson-core-2.8.8.jar;lib/jackson-annotations-2.8.8.jar --module-path mods;out -m boo
+ks/demo.Main
