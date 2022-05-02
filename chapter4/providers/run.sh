ee14530d3f1fca711bb6e56c5c721fd35ff9dfc5
Thimoty Barbieri <thimoty@thimoty.it>
2022-05-02 15:52:47 +0200

    corrected runsh

--- chapter4/providers/run.sh
+++ chapter4/providers/run.sh
@@ -1,4 +1,5 @@
 mkdir -p mods
 
-$JAVA_HOME/bin/javac --module-source-path src -d mods -m main,provider.method.example,provider.factory.example
-$JAVA_HOME/bin/java --module-path mods -m main/javamodularity.providers.main.Main
+javac --module-source-path src -d mods -m main,provider.method.example,provider.factory.example
+java --module-path mods -m main/javamodularity.providers.main.Main ../exampletext.txt
+
